123b1c19e2ccfc329f276540608498ff3c74a634
Balakrishna Peesala <balapeesala1022@gmail.com>
2024-12-01 17:11:28 +0530

    Time: 2 ms (93.52%) | Memory: 43.8 MB (9.20%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        HashMap<Integer,Integer> s=new HashMap<>();
+      HashMap<Integer,Integer> s=new HashMap<>();
         for(int i=0;i<arr.length;i++){
-            s.put(arr[i],i);
+            if(arr[i]%2==0){
+                 s.put(arr[i],i);
+            }
+           
         }
         for(int j=0;j<arr.length;j++){
             // if(s.get(arr[j]*2)==j){
